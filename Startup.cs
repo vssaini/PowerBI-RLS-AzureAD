1716472b6e210b718b355069991cf5f53670cc83
Vikram <vs00saini@gmail.com>
2022-03-02 13:39:19 +0530

    Get email address after AD user logs in

--- Startup.cs
+++ Startup.cs
@@ -6,7 +6,7 @@ namespace PowerBiRls
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;    
+    using Microsoft.Extensions.Hosting;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc.Authorization;
     using Microsoft.Identity.Web;
@@ -24,14 +24,14 @@ namespace PowerBiRls
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // To show PII in logs
+            Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
+
             // Register AadService and PbiEmbedService for dependency injection
             services.AddScoped(typeof(AadService))
                     .AddScoped(typeof(PbiEmbedService));
 
-            // services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
-            //     .AddAzureAD(options => Configuration.Bind("AzureAd", options));
-
-            services.AddMicrosoftIdentityWebAppAuthentication(Configuration, "AzureAd");
+            services.AddMicrosoftIdentityWebAppAuthentication(Configuration, "AzureAd", subscribeToOpenIdConnectMiddlewareDiagnosticsEvents: true);
 
             services.AddControllersWithViews(options =>
             {
